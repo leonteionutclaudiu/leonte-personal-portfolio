dac3443436a5bdbedac72961b535a783953ae529
Leonte Ionut-Claudiu <leonteionut98@yahoo.com>
2023-07-01 16:11:29 +0300

    Interview Link added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 `use strict`;
+
+// elementul cu clasa "section-interview"
+const sectionInterview = document.querySelector('.section-interview');
+
+// schimare culoare la fiecare 1 secunda
+setInterval(function () {
+  sectionInterview.classList.toggle('color-transition');
+}, 1000);
